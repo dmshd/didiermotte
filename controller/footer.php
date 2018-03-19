bea13659455c38ade76661a360069bdebb283332
dmshd <daniel.muyshond@gmail.com>
2018-03-19 09:16:09 +0100

    ok

---
+++ controller/footer.php
@@ -0,0 +1,8 @@
+<!-- footer -->
+	<footer>
+		<p>© 2018 Didier Motte +32 495 20 59 34 - dmotte@skynet.be</p>
+		<p>Décorateur, Antiquités, Aménagement d'intérieurs, Belgique</p>
+		<p>Restez à jour via: Facebook  Instagram</p>
+		<p>Recopié à la main avec amour par <a href="https://github.com/dmshd" target="_blank">dmshd</a></p>
+	</footer>
+<!-- endfooter -->
---
+++ view/branding.landing.view.php
@@ -0,0 +1,12 @@
+<header>
+	 	<div id="branding-landing">
+	 		<a href="#"><img id="didier-motte-logo" src="public/img/didier-motte-logo.png" alt="Didier Motte logo" title="Didier Motte logo"></a>
+	 		<p id="tagline">Antiquités • Décoration • Aménagements d'intérieur</p>
+	 		<p id="contact-info">☎ <a href="#">+32 495/20.59.34</a></p>
+	 		<ul id="menu-menu-principal" class="flex">
+	 			<li><a href="presentation.php">Présentation</a></li>
+	 			<li><a href="stock.php">Le stock</a></li>
+	 			<li><a href="contact.php">Contact</a></li>
+	 		</ul>
+	 	</div>
+ </header>
